14a5936b762c52460174703d1cf22168a6bacfb3
Akarys42 <Akarys42@users.noreply.github.com>
2020-01-05 17:21:34 +0100

    Give timezone requirements a proper name

--- tests/test_tz.py
+++ tests/test_tz.py
@@ -2,7 +2,7 @@ from tests.test_unit import TestRequirements
 from datetime import datetime as dt, timedelta as td, timezone as tz
 
 
-class TestBasicRequirements(TestRequirements):
+class TestTimezoneRequirements(TestRequirements):
     cases = (
         ('2017-05-06', dt(2017, 5, 6)),
         ('2017-05-06Z', dt(2017, 5, 6, tzinfo=tz.utc)),
